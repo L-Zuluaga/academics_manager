5af991b23b7d185259a4f39b55d4f89f98645616
Lorenzo Zuluaga <32212593+L-Zuluaga@users.noreply.github.com>
2022-09-10 11:29:54 -0500

    Add dummy comment
    Add test comment

--- lib/academics_managers_web/router.ex
+++ lib/academics_managers_web/router.ex
@@ -20,18 +20,7 @@ defmodule AcademicsManagersWeb.Router do
     get "/", PageController, :index
     resources "/signatures", SignatureController
     resources "/students", StudentController
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", AcademicsManagersWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
+    ## Test commentn yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
